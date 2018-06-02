00a3854717bbfdaf621c89fc10b44fc754f1c0ee
vitor <vitor@DESKTOP-007NIKV>
2018-06-02 18:44:40 -0300

    Cliente

---
+++ src/main/java/conexao/MainConstants.java
@@ -0,0 +1,15 @@
+package conexao;
+
+
+public class MainConstants {
+
+    //URL de conexão com o  banco de dados.
+    public static final String DB_ADDRESS = "jdbc:derby://localhost:1527/headDB";
+
+    //LOGIN de conexão com o banco de dados.
+    public static final String DB_USER = "head";
+
+    //SENHA de conexão com banco de dados.
+    public static final String DB_PASS = "head";
+
+}
