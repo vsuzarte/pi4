0f1fb50940fafe84fba82cb55c0ac0a6471221ad
vitor <vitor@DESKTOP-007NIKV>
2018-06-02 20:05:58 -0300

    venda item venda

---
+++ src/main/java/model/Venda.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Venda {
+
+    private int id;
+    private int idCliente;
+    private double valor;
+    private float total;
+    
+    
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+}
